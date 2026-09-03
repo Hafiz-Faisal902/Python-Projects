90d583d3d7271f3797e64373e7603da288463e39
Hafiz-Faisal902 <dm558522@gmail.com>
2026-09-03 17:51:03 +0500

    Rock Paper Scissors game added
    a Rock Paper Scissors game just added
    using simple if else and list

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from Programs.Guess_Number.main import Guess_Number
 
 while True:
     print("\n--- Main Menu ---")
-    print("Available options: Calculator, Exit, Help")
+    print("Available options: Calculator, Guess Number, Exit, Help")
     userplay = input("Which game do you want to play? ").strip().lower()
 
     if userplay == "exit":
         print("Thanks for playing! Goodbye.")
 
     elif userplay == "help":
-        print("Available options: Calculator, Exit, Help")
+        print("Available options: Calculator, Guess Number, Exit, Help")
         print("Type the name of the game you want to play or 'exit' to quit.")
 
-    elif userplay == "calculator":
+    elif userplay == "calculator".strip().lower():
         Calculator()
 
-    elif userplay == "guess number":
+    elif userplay == "guess number".strip().lower():
         Guess_Number()
+
+    elif userplay == "rock paper scissors".strip().lower():
+        Rock_Paper_Scissors()
 
     else:
         print("Game not found. Please choose a valid option from the menu or type help.")
